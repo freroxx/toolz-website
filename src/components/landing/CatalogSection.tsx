70847199504ea14a9e92b3cbbf965278048d889d
freroxx <yikni2829@gmail.com>
2026-08-10 13:44:53 +0100

    UPDATE AAAA

--- src/components/landing/CatalogSection.tsx
+++ src/components/landing/CatalogSection.tsx
@@ -62,7 +62,7 @@ const CatalogSection = () => {
             >
               Hardware Intelligence, <br />
               <span className="m3-gradient-text italic font-serif">resolved.</span>
-            </h2>
+            </motion.h2>
 
             <motion.p
               initial={{ opacity: 0, y: 20 }}
